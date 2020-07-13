8e376715c63f94cf75ad6f2a6ed21786c268556c
jonowu <53251695+jonowu@users.noreply.github.com>
2020-07-14 00:40:18 +1000

    feat: get login working

--- screens/login.js
+++ screens/login.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { StyleSheet } from 'react-native';
 
+import { firebase } from '../firebase/config';
+
 export default function LoginScreen({ navigation }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -10,7 +12,32 @@ export default function LoginScreen({ navigation }) {
     navigation.navigate('Registration');
   };
 
-  const onLoginPress = () => {};
+  const onLoginPress = () => {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((response) => {
+        const uid = response.user.uid;
+        const usersRef = firebase.firestore().collection('users');
+        usersRef
+          .doc(uid)
+          .get()
+          .then((firestoreDocument) => {
+            if (!firestoreDocument.exists) {
+              alert('User does not exist anymore.');
+              return;
+            }
+            const user = firestoreDocument.data();
+            navigation.navigate('Home', { user });
+          })
+          .catch((error) => {
+            alert(error);
+          });
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  };
 
   return (
     <View style={styles.container}>
