f34e0df767d20e9962909ba8ecbca2e318666ec4
jonowu <53251695+jonowu@users.noreply.github.com>
2020-07-28 16:30:50 +1000

    feat: pass back newCompletedActions as parameter

--- screens/action/index.js
+++ screens/action/index.js
@@ -4,10 +4,9 @@ import { Text, Button } from 'react-native';
 import { firebase } from '../../firebase/config';
 import Screen from '../../components/screen';
 
-function ActionScreen({ route }) {
+function ActionScreen({ route, navigation }) {
   const { id, title, body, userId, completedActions } = route.params;
 
-  console.log(completedActions);
   function completeAction() {
     const newCompletedActions = completedActions.push(id);
 
@@ -18,16 +17,14 @@ function ActionScreen({ route }) {
       .doc(userId)
       .update({
         completedActions: db.FieldValue.arrayUnion(id),
-      });
-
-    console.log(newCompletedActions);
+      })
+      .then(() => navigation.navigate('Actions', { completedActions: newCompletedActions }));
   }
 
   return (
     <Screen style={{ justifyContent: 'center' }}>
       <Text>{title}</Text>
       <Text>{body}</Text>
-      <Text>{userId}</Text>
       <Button title="Mark as complete" onPress={() => completeAction()} />
     </Screen>
   );
