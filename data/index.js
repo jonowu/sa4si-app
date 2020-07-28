527aa5d94e5ed79168af1d11462d5363f7be1700
jonowu <53251695+jonowu@users.noreply.github.com>
2020-07-28 18:09:08 +1000

    feat: add sdg 2, use hex color codes

--- data/index.js
+++ data/index.js
@@ -1,9 +1,15 @@
 const sdgs = [
   {
     number: '1',
-    name: 'No Poverty',
-    description: 'Economic growth must be inclusive to provide sustainable jobs and promote equality.',
-    color: 'red',
+    name: 'No poverty',
+    description: 'End poverty in all its forms everywhere.',
+    color: '#E5243B',
+  },
+  {
+    number: '2',
+    name: 'Zero hunger',
+    description: 'End hunger, achieve food security and improved nutrition, and promote sustainable agriculture.',
+    color: '#DDA63A',
   },
 ];
 
