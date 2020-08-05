a8577bd8495bbd8d56b1e7f6766c7dc57a0843e1
Ben Yong <53241741+BenjaminYong@users.noreply.github.com>
2020-08-05 16:36:44 +1000

    feat: populated rest of the 17 SDG data (#1)

--- data/index.js
+++ data/index.js
@@ -1,15 +1,110 @@
 const sdgs = [
   {
     number: '1',
-    name: 'No poverty',
+    name: 'No Poverty',
     description: 'End poverty in all its forms everywhere.',
     color: '#E5243B',
   },
   {
     number: '2',
-    name: 'Zero hunger',
+    name: 'Zero Hunger',
     description: 'End hunger, achieve food security and improved nutrition, and promote sustainable agriculture.',
     color: '#DDA63A',
+  },
+  {
+    number: '3',
+    name: 'Good Health and Well-being',
+    description: 'Ensure healthy lives and promote well-being for all at all ages.',
+    color: '#4C9F38',
+  },
+  {
+    number: '4',
+    name: 'Quality Education',
+    description: 'Ensure inclusive and equitable quality education and promote lifelong opportunities for all.',
+    color: '#C5192D',
+  },
+  {
+    number: '5',
+    name: 'Gender Equality',
+    description: 'Achieve gender equality and empower all women and girls.',
+    color: '#FF3A21',
+  },
+  {
+    number: '6',
+    name: 'Clean Water and Sanitation',
+    description: 'Ensure availability and sustainable management of water and sanitation for all.',
+    color: '#26BDE2',
+  },
+  {
+    number: '7',
+    name: 'Affordable and Clean Energy',
+    description: 'Ensure access to affordable, reliable, sustainable and modern energy for all.',
+    color: '#FCC30B',
+  },
+  {
+    number: '8',
+    name: 'Decent Work and Economic Growth',
+    description:
+      'Promote sustained, inclusive and sustainable economic growth, full and productive employment and decent work for all.',
+    color: '#A21942',
+  },
+  {
+    number: '9',
+    name: 'Industry, Innovation and Infrastructure',
+    description:
+      'Build resilient infrastructure, promote inclusive and sustainable industrialization and foster innovation.',
+    color: '#FD6925',
+  },
+  {
+    number: '10',
+    name: 'Reduced Inqualities',
+    description: 'Reduce inequality within and among countries.',
+    color: '#DD1367',
+  },
+  {
+    number: '11',
+    name: 'Sustainable Cities and Communities',
+    description: 'Make cities and human settlements inclusive, safe, resilient and sustainable.',
+    color: '#FD9D24',
+  },
+  {
+    number: '12',
+    name: 'Responsible Consumption and Production',
+    description: 'Ensure sustainable consumption and production patterns.',
+    color: '#BF8B2E',
+  },
+  {
+    number: '13',
+    name: 'Climate Action',
+    description: 'Take urgent action to combat climate change and its impacts.',
+    color: '#3F7E44',
+  },
+  {
+    number: '14',
+    name: 'Life below Water',
+    description: 'Conserve and sustainably use the oceans, seas and marine resources for sustainable development.    ',
+    color: '#0A97D9',
+  },
+  {
+    number: '15',
+    name: 'Life on Land',
+    description:
+      'Protect, restore and promote sustainable use of terrestrial ecosystems, sustainably manage forests, combat desertification, and halt and reverse land degradation and halt biodiversity loss.',
+    color: '#56C02B',
+  },
+  {
+    number: '16',
+    name: 'Peace, Justice and Strong Institutions',
+    description:
+      'Promote peaceful and inclusive societies for sustainable development, provide access to justice for all and build effective, accountable and inclusive institutions at all levels.',
+    color: '#00689D',
+  },
+  {
+    number: '17',
+    name: 'Partnership for the Goals',
+    description:
+      'Strengthen the means of implementation and revitalize the Global Partnership for Sustainable Development.',
+    color: '#19486A',
   },
 ];
 
