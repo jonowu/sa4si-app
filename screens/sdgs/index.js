e3daf05793f9cec5c9bd41445d83ecc23658e111
Ben Yong <53241741+BenjaminYong@users.noreply.github.com>
2020-08-06 15:48:26 +1000

    feat: adding styling to SDG tiles screen (#2)
    * feat: adding styling to SDG tiles screen
    
    Requires IOS testing
    
    * refactor: removed redundancies and converted inline styles to styled-components

--- screens/sdgs/index.js
+++ screens/sdgs/index.js
@@ -1,18 +1,37 @@
-import { Text, TouchableOpacity } from 'react-native';
+import { Text, TouchableOpacity, ScrollView } from 'react-native';
 import React from 'react';
+import styled from 'styled-components';
 
 import { sdgs } from '../../data';
 import Screen from '../../components/screen';
 
+const TileContainer = styled(TouchableOpacity)`
+  background-color: ${(props) => props.color};
+  height: 100px;
+  justify-content: center;
+  align-items: center;
+  margin: 5px;
+`;
+
+const TileText = styled(Text)`
+  color: white;
+  font-size: 25px;
+  margin-horizontal: 15px;
+`;
+
 function SdgsScreen({ navigation }) {
   return (
     <Screen style={{ justifyContent: 'center' }}>
-      <Text>Tap on an SDG to learn more!</Text>
-      {sdgs.map((sdg, i) => (
-        <TouchableOpacity key={i} onPress={() => navigation.navigate('SDG', { sdg: sdg })}>
-          <Text>{sdg.name}</Text>
-        </TouchableOpacity>
-      ))}
+      <Text style={{ padding: 5 }}>Tap on an SDG to learn more!</Text>
+      <ScrollView>
+        {sdgs.map((sdg, i) => (
+          <TileContainer color={sdg.color} key={i} onPress={() => navigation.navigate('SDG', { sdg: sdg })}>
+            <TileText adjustsFontSizeToFit numberOfLines={1} allowFontScaling>
+              {sdg.name}
+            </TileText>
+          </TileContainer>
+        ))}
+      </ScrollView>
     </Screen>
   );
 }
