0924d733aea61e4a0a8869b911f17b4e6ec52419
Iansong95 <69234613+Iansong95@users.noreply.github.com>
2020-08-16 21:00:13 +1000

    feat: updated individual sdg page format (#5)
    * feat: updated individual sdg page format
    
    * feat: switched from stylesheet to styled-components
    
    * fix: update styling
    
    Co-authored-by: jonowu <53251695+jonowu@users.noreply.github.com>

--- screens/sdg/index.js
+++ screens/sdg/index.js
@@ -1,16 +1,44 @@
-import { Text } from 'react-native';
+import { Text, ScrollView } from 'react-native';
 import React from 'react';
+import styled from 'styled-components/native';
 
 import Screen from '../../components/screen';
+
+const HeaderContainer = styled.View`
+  background-color: ${({ color }) => color};
+`;
+
+const Heading = styled.Text`
+  color: white;
+  padding: 60px 20px;
+  font-size: 20px;
+  font-weight: 700;
+  margin-top: -20px; // should match the margin-top value of BodyContainer
+`;
+
+const BodyContainer = styled.View`
+  flex: 1;
+  padding: 20px;
+  background-color: white;
+  border-radius: 20px;
+  margin-top: -20px; // so that the container overlaps
+`;
 
 function Sdg({ route }) {
   const { sdg } = route.params;
 
   return (
-    <Screen style={{ alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Goal {sdg.number}</Text>
-      <Text>{sdg.name}</Text>
-      <Text>{sdg.description}</Text>
+    <Screen>
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <HeaderContainer color={sdg.color}>
+          <Heading>
+            {sdg.number}. {sdg.name}
+          </Heading>
+        </HeaderContainer>
+        <BodyContainer>
+          <Text>{sdg.description}</Text>
+        </BodyContainer>
+      </ScrollView>
     </Screen>
   );
 }
