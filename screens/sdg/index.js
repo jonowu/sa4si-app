8e50a6eb32751b6bc812de80e48900600caf598a
Iansong95 <69234613+Iansong95@users.noreply.github.com>
2020-08-18 18:26:45 +1000

    feat: add sdg images (#10)
    * created ./static/img/sdgs/ and added sdg images
    
    * feat: added images for individual sdg page
    
    * fix: added img src to sdgs.js, removed switch statement

--- screens/sdg/index.js
+++ screens/sdg/index.js
@@ -1,26 +1,14 @@
-import { Text, ScrollView } from 'react-native';
+import { Text, ScrollView, Image } from 'react-native';
 import React from 'react';
 import styled from 'styled-components/native';
-
 import Screen from '../../components/screen';
-
-const HeaderContainer = styled.View`
-  background-color: ${({ color }) => color};
-`;
-
-const Heading = styled.Text`
-  color: white;
-  padding: 60px 20px;
-  font-size: 20px;
-  font-weight: 700;
-  margin-top: -20px; // should match the margin-top value of BodyContainer
-`;
 
 const BodyContainer = styled.View`
   flex: 1;
   padding: 20px;
   background-color: white;
-  border-radius: 20px;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
   margin-top: -20px; // so that the container overlaps
 `;
 
@@ -28,13 +16,18 @@ function Sdg({ route }) {
   const { sdg } = route.params;
 
   return (
-    <Screen>
+    <Screen style={{ backgroundColor: sdg.color }}>
       <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-        <HeaderContainer color={sdg.color}>
-          <Heading>
-            {sdg.number}. {sdg.name}
-          </Heading>
-        </HeaderContainer>
+        <Image
+          source={require('../../static/img/sdgs/SDG_logo.png')}
+          resizeMode="contain"
+          style={{ margin: 10, backgroundColor: 'white', height: '7%', width: '95%' }}
+        />
+        <Image
+          source={sdg.src}
+          resizeMode="contain"
+          style={{ margin: 10, marginTop: -10, height: '25%', width: '40%' }}
+        />
         <BodyContainer>
           <Text>{sdg.description}</Text>
         </BodyContainer>
