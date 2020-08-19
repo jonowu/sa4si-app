c190d3c8a463ba589ff1a6ac5e7fa4b936f14520
Ben Yong <53241741+BenjaminYong@users.noreply.github.com>
2020-08-19 10:43:20 +1000

    feat: News loading and markdown support in articles (#11)
    * feat: loading added to news feed
    
    * feat: markdown support on articles
    
    * feat: styling improvements to articles

--- screens/article/index.js
+++ screens/article/index.js
@@ -1,21 +1,35 @@
 import { Text, Image } from 'react-native';
 import React from 'react';
+
 import Moment from 'moment';
+import Markdown from 'react-native-markdown-display';
+import styled from 'styled-components/native';
 
 import Screen from '../../components/screen';
+import { ScrollView } from 'react-native-gesture-handler';
+
+const ArticleHeader = styled.View`
+  align-items: center;
+  height: 40%;
+  margin: 8px;
+`;
 
 function Article({ route }) {
   const { article } = route.params;
   const { id, title, description, body, created_at, image } = article;
 
   return (
-    <Screen style={{ alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Article {id}</Text>
-      <Text>{title}</Text>
-      <Text>{description}</Text>
-      <Text>{body}</Text>
-      <Text>{Moment(created_at).format('DD/MM/YYYY hh:mm A')}</Text>
-      {image && <Image style={{ width: '80%', height: '30%' }} source={{ uri: image.formats.small.url }}></Image>}
+    <Screen>
+      <ArticleHeader>
+        <Text>Article {id}</Text>
+        <Text>{title}</Text>
+        <Text>{description}</Text>
+        <Text>{Moment(created_at).format('DD/MM/YYYY hh:mm A')}</Text>
+        {image && <Image style={{ width: '50%', height: '70%' }} source={{ uri: image.formats.small.url }}></Image>}
+      </ArticleHeader>
+      <ScrollView contentContainerStyle={{ padding: 20 }}>
+        <Markdown>{body}</Markdown>
+      </ScrollView>
     </Screen>
   );
 }
--- screens/news/index.js
+++ screens/news/index.js
@@ -1,18 +1,19 @@
-import { Text, View, Image, TouchableOpacity, ScrollView } from 'react-native';
+import { Text, ScrollView, ActivityIndicator } from 'react-native';
 import React, { useState, useEffect, useContext } from 'react';
+
 import axios from 'axios';
-import styled from 'styled-components';
 import Moment from 'moment';
+import styled from 'styled-components/native';
 
 import { AuthenticatedContext } from '../../context/authenticated-context';
 import Screen from '../../components/screen';
 import { api } from '../../data';
 
-const TileList = styled(View)`
+const TileList = styled.View`
   margin: 20px;
 `;
 
-const TileContainer = styled(TouchableOpacity)`
+const TileContainer = styled.TouchableOpacity`
   height: 250px;
   width: 100%;
   flex-direction: column;
@@ -21,45 +22,46 @@ const TileContainer = styled(TouchableOpacity)`
   margin-bottom: 20px;
 `;
 
-const TileImage = styled(Image)`
+const TileImage = styled.Image`
   height: 47%;
 `;
 
-const TileAltImage = styled(View)`
+const TileAltImage = styled.View`
   height: 47%;
   background-color: palegreen;
 `;
 
-const TileText = styled(View)`
+const TileText = styled.View`
   height: 53%;
   color: black;
   background-color: #e5e5e5;
 `;
 
-const TileTextTop = styled(View)`
+const TileTextTop = styled.View`
   flex-direction: row;
   justify-content: space-between;
 `;
 
-const TileTextTopDate = styled(Text)`
+const TileTextTopDate = styled.Text`
   font-size: 12px;
   margin-right: 20px;
   margin-top: 25px;
 `;
 
-const TileTextTopTitle = styled(Text)`
+const TileTextTopTitle = styled.Text`
   font-size: 22px;
   font-weight: bold;
   margin-left: 20px;
   margin-top: 15px;
 `;
 
-const TileTextDescription = styled(Text)`
+const TileTextDescription = styled.Text`
   font-size: 17px;
   margin: 20px 20px;
 `;
 
 function NewsScreen({ navigation }) {
+  const [loading, setLoading] = useState(true);
   const [news, setNews] = useState([]);
 
   const authContext = useContext(AuthenticatedContext);
@@ -74,6 +76,7 @@ function NewsScreen({ navigation }) {
       })
       .then((response) => {
         setNews(response.data);
+        setLoading(false);
       })
       .catch((error) => {
         alert(error.response.data.error);
@@ -83,27 +86,31 @@ function NewsScreen({ navigation }) {
 
   return (
     <Screen style={{ alignItems: 'center', justifyContent: 'center' }}>
-      <Text style={{ padding: 5 }}>Tap on an article to learn more!</Text>
-      <ScrollView style={{ width: '100%' }}>
-        <TileList>
-          {news.map((article, i) => (
-            <TileContainer key={i} onPress={() => navigation.navigate('Article', { article: article })}>
-              {article.image ? (
-                <TileImage source={{ uri: article.image.formats.small.url }}></TileImage>
-              ) : (
-                <TileAltImage></TileAltImage>
-              )}
-              <TileText>
-                <TileTextTop>
-                  <TileTextTopTitle>{article.title}</TileTextTopTitle>
-                  <TileTextTopDate>{Moment(article.created_at).format('DD/MM/YY')}</TileTextTopDate>
-                </TileTextTop>
-                <TileTextDescription>{article.description}</TileTextDescription>
-              </TileText>
-            </TileContainer>
-          ))}
-        </TileList>
-      </ScrollView>
+      {loading ? (
+        <ActivityIndicator size="large"></ActivityIndicator>
+      ) : (
+        <ScrollView style={{ width: '100%' }}>
+          <Text style={{ padding: 5, textAlign: 'center' }}>Tap on an article to learn more!</Text>
+          <TileList>
+            {news.map((article, i) => (
+              <TileContainer key={i} onPress={() => navigation.navigate('Article', { article: article })}>
+                {article.image ? (
+                  <TileImage source={{ uri: article.image.formats.small.url }}></TileImage>
+                ) : (
+                  <TileAltImage></TileAltImage>
+                )}
+                <TileText>
+                  <TileTextTop>
+                    <TileTextTopTitle>{article.title}</TileTextTopTitle>
+                    <TileTextTopDate>{Moment(article.created_at).format('DD/MM/YY')}</TileTextTopDate>
+                  </TileTextTop>
+                  <TileTextDescription>{article.description}</TileTextDescription>
+                </TileText>
+              </TileContainer>
+            ))}
+          </TileList>
+        </ScrollView>
+      )}
     </Screen>
   );
 }
